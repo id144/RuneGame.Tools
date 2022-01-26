f9b61130f3ea4a6141b0ba5fda91547b8521b12d
Andrej Boleslavsky <yer@atlas.cz>
2022-01-26 22:18:11 +0100

    Fixed contract address and transaction address

--- SolidityRiddle/messageSignatureBruteForce.py
+++ SolidityRiddle/messageSignatureBruteForce.py
@@ -4,17 +4,18 @@ import json
 from eth_account.messages import encode_defunct
 from eth_account import Account
 from web3 import Web3
-
+from web3.middleware import geth_poa_middleware
 #https://bscscan.com/tx/0xd3b232081665b74dd780a8c2633c454c2d682e05be2a8f2e999e81b97fe96438
 
 def main(_tx):
     w3 = Web3(Web3.HTTPProvider('https://bsc-dataseed1.binance.org:443'))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)    
     try:
         tx = w3.eth.get_transaction(_tx)
     except Exception as x:
         print(x)
 
-    riddleAddress = Web3.toChecksumAddress('0x50D1B6D61b57581AaB00D248F81a1E8170a7f3aE')
+    riddleAddress = Web3.toChecksumAddress('0xc9d4ed4bac9c9ff3c96f3e60c59d6a9a99185f3d')
     with open("./ABI/runeRiddleABI.json", "r") as read_file:
         riddleAbi = json.load(read_file)
     riddleContract = w3.eth.contract(address=riddleAddress, abi=riddleAbi)
@@ -22,7 +23,7 @@ def main(_tx):
     func_obj, func_params = riddleContract.decode_function_input(tx.input)
 
     signature = func_params['signature']
-    address = Web3.toChecksumAddress(func_obj.address)
+    address = Web3.toChecksumAddress(tx['from'])
     for i in range(0,1000000000):
         message = encode_defunct(text= str(i))
         _acc = Account.recover_message(message,signature=signature)
